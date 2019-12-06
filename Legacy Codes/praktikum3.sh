315f419c91647155a89cbf5261214e5c66c26ac5
wildany <54576798+wildany@users.noreply.github.com>
2019-12-06 14:26:38 +0700

    Create praktikum3.sh

---
+++ Legacy Codes/praktikum3.sh
@@ -0,0 +1,22 @@
+Sudo adduser dengklek
+Sudo adduser kholil
+Sudo adduser wahid
+
+Sudo addgroup satu
+Sudo addgroup dua
+
+Sudo adduser deklek satu
+Sudo adduser kholil dua
+Sudo adduser wahid dua
+
+Nano skrip1.sh
+Echo “Terserah”;
+Sudo chmod 644 skrip1.sh
+Sudo chown dengklek skrip1.sh
+./skrip1.sh
+
+Nano skrip2.sh
+Echo “Terserah”;
+Sudo chmod 700 skrip2.sh
+Sudo chown dengklek skrip2.sh
+./skrip2.sh 
